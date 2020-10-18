41562d9d66a44c7706cfb9bb846d40ee84f897dc
Matthew Vogee <mvogee@yahoo.com>
2020-10-18 08:36:54 -0700

    digital root

---
+++ digital_root.cpp
@@ -0,0 +1,12 @@
+int digital_root(int n)
+{
+  // recursively solves problem
+    if (n < 10)
+      return n;
+    int sum = 0;
+    while (n > 0) {
+      sum += n % 10;
+      n /= 10;
+    }
+    return (digital_root(sum));
+}
