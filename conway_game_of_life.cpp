eb2a9f6034b8832de73a58b7d35eb3819dee9480
Matthew Vogee <mvogee@yahoo.com>
2020-10-23 13:37:28 -0700

    progress on conway game of life

--- conway_game_of_life.cpp
+++ conway_game_of_life.cpp
@@ -57,14 +57,29 @@ void seed_life(int type) {
   }
 }
 
+bool check_live(int i, int j) {
+  return gamemap[i][j] == '#' ? true : false;
+}
+
 void game_of_life() {
-  
+  bool alive;
+  // life checking; go through each spot on the map
+
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 10; j++) {
+      alive = check_live(i, j);
+      std::cout << alive;
+      // if it is dead - check if it should live
+      // if it is alive - check if it should die
+    }
+    std::cout << '\n';
+  }
 }
 
 int main(int argc, char const *argv[]) {
   seed_life(3);
   print_map();
-  for (int itterations = 0; itterations != 10; itterations++) {
+  for (int itterations = 0; itterations != 1; itterations++) {
     game_of_life();
     print_map();
   }
