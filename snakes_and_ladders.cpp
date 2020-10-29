363a284d80bd9cdddc876d8cdb68a5da5c458f75
Matthew Vogee <mvogee@yahoo.com>
2020-10-28 19:15:07 -0700

    changed array of board locations to map of snakes and ladder locations

--- snakes_and_ladders.cpp
+++ snakes_and_ladders.cpp
@@ -1,40 +1,46 @@
 #include <sstream>
+#include <map>
+#include <iostream>
 class SnakesLadders
 {
 
   private:
-  int board[100] = {0,36,0,0,0,0,7,23,0,0,
-                    0,0,0,0,11,-10,0,0,0,0,
-                    21,0,0,0,0,0,0,56,0,0,
-                    0,0,0,0,0,8,0,0,0,0,
-                    0,0,0,0,0,-21,0,0,-38,0,
-                    16,0,0,0,0,0,0,0,0,0,
-                    0,-43,0,-4,0,0,0,0,0,0,
-                    20,0,0,-21,0,0,0,20,0,0,
-                    0,0,0,0,0,0,7,0,-21,0,
-                    0,-4,0,0,-20,0,0,0,-19,0};
+  std::map<int, int> board;
+  : board{{2,38}, {7,14}, {8,31}, {15, 26}, {16, 6}, {21, 42}, {28, 84}, {36, 44}, {46, 25}, {49, 11}, {51, 67}, {62, 19}, {64, 60}, {71, 91}, {74, 53}, {78, 98}, {87, 94}, {89, 68}, {92, 88}, {95, 75}, {99,80}};
+  int players[2] = {0, 0}; // index 0 is p1 -- index 1 is p2
+  int whosturn = 0; // starts at 2 to avoid division by 0
   public:
 
-  int players[2] = {0, 0}; // index 0 is p1 -- index 1 is p2
-  int whosturn = 2; // starts at 2 to avoid division by 0
     SnakesLadders(){};
     std::string play(int die1, int die2)
     {
       std::ostringstream o; // to format the return string
-      int p = whosturn % 2;
       if (players[0] == 100 || players[1] == 100)
         return "Game over!";
-      players[p] += die1 + die2;
-      if (players[p] > 100)
-        players[p] = 100 - (players[p] - 100);
-      if (players[p] == 100)
-        o << "Player " << p + 1 << " Wins!";
+      players[whosturn] += die1 + die2;
+      if (players[whosturn] > 100)
+        players[whosturn] = 100 - (players[whosturn] - 100);
+      if (players[whosturn] == 100)
+        o << "Player " << whosturn + 1 << " Wins!";
       else {
-        players[p] += board[players[p] - 1];
-        o << "Player " << p + 1 << " is on square " << players[p];
+        auto slide = board.find(players[whosturn]); // auto is an std::map<int, int>::iterator;
+        if (slide == board.end())
+          players[whosturn] = slide->second;
+        o << "Player " << whosturn + 1 << " is on square " << players[whosturn];
       }
 
-      if (die1 != die2) whosturn++;
+      if (die1 != die2)
+        whosturn = (whosturn == 0) ? 1 : 0;
       return o.str();
     };
 };
+
+
+int main() {
+
+  SnakesLadders game;
+
+  std::cout << game.play(1,1);
+  std::cout << game.play(6,5);
+  return 0;
+}
