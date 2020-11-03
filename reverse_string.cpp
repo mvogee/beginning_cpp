5504fd61b0b263dcc428791a1973f59a07d76aca
Matthew Vogee <mvogee@yahoo.com>
2020-11-02 22:28:03 -0800

    added out of place reversal

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -7,6 +7,7 @@ void swap(char& a, char& b) {
   b = hold;
 }
 
+// in place
 void reverse_string(std::string& str) {
   int back = str.length();
   for (int i = 0; i < back / 2; i++) {
@@ -14,12 +15,22 @@ void reverse_string(std::string& str) {
   }
 }
 
+// new string
+std::string new_reverse_string(std::string str) {
+  std::string reversed = "";
+  for (int i = str.length() - 1; i >= 0; i--)
+    reversed+= str.at(i);
+  return (reversed);
+}
+
 int main(int argc, char *argv[]) {
   if (argc > 1)
   {
     std::string mystring = argv[1];
     reverse_string(mystring);
-    std::cout << mystring;
+    std::cout << mystring << "\n";
+    mystring = new_reverse_string(mystring);
+    std::cout << mystring << "\n";
   }
   return (0);
 }
