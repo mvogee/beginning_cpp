af7c5ffe8b6d345f6fec9a18090c9a0571f53e71
Matthew Vogee <mvogee@yahoo.com>
2020-11-11 15:56:51 -0800

    passing tests

--- adding_big_numbers.cpp
+++ adding_big_numbers.cpp
@@ -1,9 +1,6 @@
 #include <string>
 #include <iostream>
-
-#include <string>
-#include <iostream>
-
+/*
 std::string add(const std::string& a, const std::string& b) {
   std::string big_num = "";
   int a_idx = a.length() - 1;
@@ -16,7 +13,7 @@ std::string add(const std::string& a, const std::string& b) {
     else {
       big_num.insert(0, std::to_string(storval % 10));
       storval /= 10;
-    }  
+    }
     a_idx--;
     b_idx--;
   }
@@ -57,4 +54,63 @@ int main() {
   std::cout << add("1234500912345678002345", "123456789") << "\n";
   std::cout << add("abcdefg", "abcdefg") << "\n";
 
+}
+ */
+
+int ctoi(char c) {
+  return (c - '0');
+}
+
+ std::string sum_strings(const std::string& a, const std::string& b) {
+  std::string product = "";
+  int aidx = a.length(),
+      bidx = b.length(),
+      holder = 0;
+      std::cout << "seg1\n";
+  while (aidx > 0 && bidx > 0) {
+    holder += ctoi(a[aidx - 1]) + ctoi(b[bidx - 1]);
+    product.insert(0, std::to_string(holder % 10));
+    holder >= 10 ? holder = 1 : holder = 0;
+    aidx--;
+    bidx--;
+  }
+  std::cout << "seg2\n";
+  if (aidx > 0) {
+    std::cout << "seg3\n";
+    while (aidx > 0) {
+      holder += ctoi(a[aidx - 1]);
+      product.insert(0, std::to_string(holder % 10));
+      holder >= 10 ? holder = 1 : holder = 0;
+      aidx--;
+    }
+  }
+  else if (bidx > 0) {
+    std::cout << "seg4\n";
+    while (bidx > 0) {
+      std::cout << "loop1\n";
+      holder += ctoi(b[bidx - 1]);
+      product.insert(0, std::to_string(holder % 10));
+      holder >= 10 ? holder = 1 : holder = 0;
+      bidx--;
+    }
+  }
+  std::cout << "seg5\n";
+  if (holder > 0) {
+    std::cout << "seg6\n";
+    product.insert(0, "1");
+    std::cout << "seg7\n";
+  }
+
+  return product;
+}
+
+
+int main() {
+  std::string str = "";
+  std::cout << str.length();
+  //std::cout << '0' - '0' << "\n";
+  //std::cout << (18 - (18 % 10)) / 10 << "\n";
+  //std::cout << sum_strings("55", "55") << "\n";
+  std::cout << sum_strings("", "10") << "\n";
+  return 0;
 }
